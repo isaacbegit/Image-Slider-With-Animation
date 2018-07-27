220373d929b46c3f637f3fdd066cd5b28befaf3c
Isaac Be <esaaco@gmail.com>
2018-07-27 22:41:11 +0200

    Add files via upload

---
+++ SlideImagesWithAnimation/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace SlideImagesWithAnimation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Slideimg = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.Slideimg)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(89)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Basic Sans Serif 7", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(797, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Slide Images with Animation";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(52)))), ((int)(((byte)(89)))));
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.Font = new System.Drawing.Font("Basic Sans Serif 7", 20F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(10, 441);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(797, 47);
+            this.label2.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Slideimg
+            // 
+            this.Slideimg.Location = new System.Drawing.Point(16, 123);
+            this.Slideimg.Name = "Slideimg";
+            this.Slideimg.Size = new System.Drawing.Size(795, 315);
+            this.Slideimg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Slideimg.TabIndex = 3;
+            this.Slideimg.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(718, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Location = new System.Drawing.Point(807, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 478);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label4.Location = new System.Drawing.Point(0, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 478);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(817, 10);
+            this.label5.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.White;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label6.Location = new System.Drawing.Point(0, 488);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(817, 10);
+            this.label6.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(203, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(396, 51);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Slide Effect";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(58, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Center";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(151, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(42, 17);
+            this.radioButton3.TabIndex = 0;
+            this.radioButton3.Text = "Roll";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(289, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(47, 17);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.Text = "Slide";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(83)))), ((int)(((byte)(52)))), ((int)(((byte)(173)))));
+            this.ClientSize = new System.Drawing.Size(817, 498);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Slideimg);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slide Images With Animation";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Slideimg)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Slideimg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ SlideImagesWithAnimation/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlideImagesWithAnimation
+{
+    public partial class Form1 : Form
+    {
+        int SliderCounter = 0;
+        string[] Imgs_slider;
+        Random rnd = new Random();
+        Util.Effect EffectType=Util .Effect .Center;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public static class Util
+        {
+            public enum Effect { Roll, Slide, Center, Blend }
+
+            public static void Animate(Control ctl, Effect effect, int msec, int angle)
+            {
+                int flags = effmap[(int)effect];
+
+                if (ctl.Visible)
+                {
+                    flags |= 0x10000; angle += 180;
+                }
+                else
+                {
+                    if (ctl.TopLevelControl == ctl) flags |= 0x20000;
+                    else if (effect == Effect.Blend) throw new ArgumentException();
+                }
+
+                flags |= dirmap[(angle % 360) / 45];
+                bool ok = AnimateWindow(ctl.Handle, msec, flags);
+                if (!ok) throw new Exception("Animation failed");
+                ctl.Visible = !ctl.Visible;
+            }
+
+            private static int[] dirmap = { 1, 5, 4, 6, 2, 10, 8, 9 };
+            private static int[] effmap = { 0, 0x40000, 0x10, 0x80000 };
+
+            [DllImport("user32.dll")]
+            private static extern bool AnimateWindow(IntPtr handle, int msec, int flags);
+        }
+
+        static string[] LstImgs(string FilePath)
+        {
+            string Imagepaths = Application.StartupPath + @"\" + FilePath;
+            DirectoryInfo d = new DirectoryInfo(Imagepaths);
+            FileInfo[] Files = d.GetFiles(); // Getting Files
+
+            string[] ret = new string[Files.Length];
+            for (int x = 0; x < Files.Length; x++)
+            {
+                ret[x] = Files[x].Name;
+            }
+            return ret;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Slideimg.Image = Image.FromFile(Application.StartupPath + @"\slides\1.jpg");// initial image at startup app
+            Imgs_slider = LstImgs("slides");// load all images names 
+        }
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int rand_effect = rnd.Next(0, Imgs_slider.Length - 1);
+
+            if (Imgs_slider.Length > SliderCounter)
+            {
+                //Sliderimg2.Image = Image.FromFile(Application.StartupPath + @"\sliders\" + Imgs_slider[rand_effect]);
+                Slideimg.Image = Image.FromFile(Application.StartupPath + @"\slides\" + Imgs_slider[SliderCounter]);
+                Util.Animate(Slideimg, EffectType, 250, 360);
+                SliderCounter += 1;
+
+            }
+            else
+            {
+                SliderCounter = 0;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                EffectType = Util.Effect.Center;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                EffectType = Util.Effect.Roll;
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                EffectType = Util.Effect.Slide ;
+            }
+        }
+    }
+}
